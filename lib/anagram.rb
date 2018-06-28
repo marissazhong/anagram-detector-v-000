6c31ea0748a6cc494cba2711e1a479bb6a1451e0
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 15:49:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+  def match(matches)
+    @word = @word.split('').sort
+    matches.map! {|word| word.split('').sort != @word ? word = 'no match' : word}
+    matches = matches.reject {|word| word == 'no match'}
+  end
+end
+
+#diaper = Anagram.new('diaper')
+#diaper.match(%w(hello world zombies pants diaper dipper))
