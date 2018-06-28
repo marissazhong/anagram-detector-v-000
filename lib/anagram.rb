730a238f0a41729454afb5eaf14e0b62fbb08169
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 15:19:20 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,7 +5,7 @@ class Anagram
     @word = word
   end
   def match(words)
-    words.map! {|word| word.length == @word.length ? word.split('').sort : delete(word)}
+    words.map! {|word| word.length == @word.length ? word.split('').sort : words.delete(word)}
     puts words
   end
 end
